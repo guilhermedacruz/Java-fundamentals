df7efd05154ac7e6e753cd1d987d32b98fa67b4e
guiro7419 <44686224+guiro7419@users.noreply.github.com>
2019-05-29 20:52:04 -0300

    Adicionei arquivos via upload

---
+++ Fundamentos/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author guilherme.crsilva
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
